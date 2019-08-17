175ef2957ce4c90982528b9f55c1403f48cafc1e
Gregory Perez <gregoryperezfigueredo@gmail.com>
2019-08-17 17:10:28 -0400

    se finalizó sección 08 del curso de nodejs

--- 07-restserver/server/server.js
+++ 07-restserver/server/server.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
  
 app.get('/usuario', function (req, res) {
-	res.json('get Usuario');
+	res.json('get Usuario LOCAL!!');
 });
 
 app.post('/usuario', function (req, res) {
