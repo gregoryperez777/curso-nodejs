7d13d80d1cff607c4bcacca560993cd9c6b70d93
Gregory <gregory@rene.md>
2020-03-05 18:51:55 -0400

    Se configuro socketio del lado del servidor

---
+++ 08-sockets-basico/server/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const sockeIO = require('socket.io');
+
+// se importa http porque socketIO no trabaja 
+// con el servidor creado con const app = express();
+// sino con el servidor nativo de NodeJs creado 
+// a traves de http
+// Nota: el paquete http ya viene con nodejs
+// no hay que colocarlo en el package.json 
+const http = require('http');
+
+const path = require('path');
+
+const app = express();
+let server = http.createServer(app);
+
+const publicPath = path.resolve(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+
+// io es la comunicacion con el backend
+
+let io = sockeIO(server);
+
+server.listen(port, (err) => {
+
+    if (err) throw new Error(err);
+
+    console.log(`Servidor corriendo en puerto ${ port }`);
+
+});
